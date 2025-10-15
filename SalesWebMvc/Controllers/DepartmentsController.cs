94f97476bb46b264f204a93afe1a4e8c36cfff6d
Bronsilber teixeira <bron.teixeira.s@gmail.com>
2025-10-15 12:02:35 -0300

    Primeiro mvc Departments

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Models.Department> list = new List<Models.Department>();
+            list.Add(new Models.Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Models.Department { Id = 1, Name = "Books" });
+            return View(list);
+        }
+    }
+}
